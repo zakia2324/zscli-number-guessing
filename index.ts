bc22f5962bcd28eaf49f0cbefdb547dc0abf1203
zakia2324 <160940055+zakia2324@users.noreply.github.com>
2024-03-25 01:39:49 +0500

    created number guessing game

---
+++ index.ts
@@ -0,0 +1,21 @@
+#!usr/bin/env Node
+import inquirer from "inquirer"
+
+const randomNumber=Math.floor(Math.random() * 6 + 1);
+const answers = await inquirer.prompt([
+
+    {
+
+    name : "userGuessedNumber",
+    type:  "number",
+    message : "Please guess a number between 1-6: ",
+},
+
+]);
+
+if (answers.userGuessedNumber===randomNumber){
+    console.log("congratulations! you guessed  right number.");
+}
+else{
+    console.log("you guessed wrong number");
+}
